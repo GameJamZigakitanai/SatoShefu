e1a9ec8aa0a7541851f413d03f315ee4ee1e449d
gamejam14 <gamejam14>
2016-02-20 19:59:43 +0900

    add result button

---
+++ project/assets/Script/result/resultButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resultButton : MonoBehaviour {
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        sceneManager.NextScene("Title");
+    }
+}
+
