c19e4d5d88f8e17265aaa7a91c1d0c9e5fd3287e
gamejam14 <gamejam14>
2016-02-20 21:41:53 +0900

    fix result ui(totyuu)

---
+++ project/assets/Script/result/rankText.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;  ////ここを追加////
+
+public class rankText : MonoBehaviour {
+    // ランクの表示
+    //--------------------------------------------------------------------
+    void OnEnable()
+    {
+        int score = result.Instance.Score;
+
+        //講評一覧を表示
+        //（くそまずい:E）
+        if (score < 100)
+        {
+            this.GetComponent<Text>().text = "F";
+        }
+        //(まずい）
+        else if (score < 200)
+        {
+            this.GetComponent<Text>().text = "E";
+        }
+        //(おいしくない）
+        else if (score < 300)
+        {
+            this.GetComponent<Text>().text = "D";
+        }
+        //(ふつう）
+        else if (score < 400)
+        {
+            this.GetComponent<Text>().text = "C";
+        }
+        //(おいしい）
+        else if (score < 500)
+        {
+            this.GetComponent<Text>().text = "B";
+        }
+        //(超美味しい）
+        else if (score < 600)
+        {
+            this.GetComponent<Text>().text = "A";
+        }
+        //(神）
+        else
+        {
+            this.GetComponent<Text>().text = "S";
+        }
+    }
+}
--- project/assets/Script/result/reviewText.cs
+++ project/assets/Script/result/reviewText.cs
@@ -12,37 +12,37 @@ public class reviewText : MonoBehaviour {
 
         //講評一覧を表示
         //（くそまずい）
-        if(score < -100)
+        if(score < 100)
         {
             this.GetComponent<Text>().text = "講評：くそまずい！！\n"
                 +"今まで食べた\nカレーの中で\n一番まずい！";
         }
         //(まずい）
-        else if (score < 0)
+        else if (score < 200)
         {
             this.GetComponent<Text>().text = "講評：まずい！\n"
                 + "こんなカレーを\n出すなんて\n信じられない！";
         }
         //(おいしくない）
-        else if (score < 100)
+        else if (score < 300)
         {
             this.GetComponent<Text>().text = "講評：おいしくない\n"
                 + "期待していた味と\n全然違う";
         }
         //(ふつう）
-        else if (score < 200)
+        else if (score < 400)
         {
             this.GetComponent<Text>().text = "講評：普通\n"
                 + "レトルトカレーと\nいい勝負";
         }
         //(おいしい）
-        else if (score < 300)
+        else if (score < 500)
         {
             this.GetComponent<Text>().text = "講評：おいしい\n"
                 + "このカレーなら\n毎日食べたい！";
         }
         //(超美味しい）
-        else if (score < 400)
+        else if (score < 600)
         {
             this.GetComponent<Text>().text = "講評：超美味しい\n"
                 + "今まで食べた\nカレーの中で\n一番美味しい！";
