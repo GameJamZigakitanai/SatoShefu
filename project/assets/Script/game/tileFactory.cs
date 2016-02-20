f4621ba7faa625ce26b59900be50a37a0e89fbb7
AyumiYasui <anzu.ya0627@gmail.com>
2016-02-20 16:42:09 +0900

    add タイル.png

--- project/assets/Script/game/tileFactory.cs
+++ project/assets/Script/game/tileFactory.cs
@@ -7,6 +7,8 @@ public class tileFactory : MonoBehaviour
 	// メンバ変数
 	//--------------------------------------------------------------------
 	[SerializeField]
+	private float start_y;              // 初めの高さ
+	[SerializeField]
 	private int width_tile;             // 横タイル数
 	[SerializeField]
 	private int height_tile;            // 縦タイル数
@@ -19,17 +21,32 @@ public class tileFactory : MonoBehaviour
 	//--------------------------------------------------------------------
 	void Awake()
 	{
-		// タイルの大きさを取得する
+		// 画面の座標を取得する
+		Rect screen = new Rect();
+		{
+			var camera = Camera.main;
+			var top_left = camera.ScreenToWorldPoint(Vector3.zero);
+
+			screen.x =  top_left.x;
+			screen.y = -top_left.y;
+
+			var bottom_light = camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+			screen.size = new Vector2(bottom_light.x - top_left.x, -bottom_light.y - top_left.y);
+		}
+
+		// タイルの大きさを作る
 		Vector2 size;
 		{
 			var sprite = prefab.gameObject.GetComponent<SpriteRenderer>();
-			var rect = sprite.sprite.rect;
-			size.x = rect.width;
-			size.y = rect.height;
+			var rect = sprite.bounds;
+			size.x = rect.size.x;
+			size.y = rect.size.y;
 		}
 
-		Vector2 init_pos = new Vector2(size.x * 0.5f + margin_tile_to_tile, size.y * 0.5f + margin_tile_to_tile);
-		Vector2 pos = new Vector2(init_pos.x,init_pos.y);
+		Vector2 init_pos = new Vector2();
+		init_pos.x = margin_tile_to_tile + screen.x + size.x * 0.5f;
+		init_pos.y = screen.y - margin_tile_to_tile - start_y - size.y * 0.5f;
+        Vector2 pos = new Vector2(init_pos.x,init_pos.y);
 
 		// 左上から生成していく
 		for (int y = 0; y < height_tile; ++y)
@@ -54,6 +71,6 @@ public class tileFactory : MonoBehaviour
 	{
 		var new_tile = GameObject.Instantiate(prefab);
 		new_tile.transform.parent = this.transform;
-		new_tile.transform.localPosition = _pos;
+		new_tile.transform.position = _pos;
 	}
 }
