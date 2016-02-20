60d928e05144baa78e9ce1a0e08dbc1c6ad0ec21
AyumiYasui <anzu.ya0627@gmail.com>
2016-02-20 21:40:56 +0900

    add title

---
+++ project/assets/Script/title/logo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logo : MonoBehaviour {
+
+	// メンバ変数
+	//--------------------------------------------------------------------
+	public readonly float move_height = 0.4f;
+
+	private float default_y;
+	private float move;
+
+	// @brief  : 初期化
+	//--------------------------------------------------------------------
+	void Awake()
+	{
+		default_y = transform.position.y;
+		move = 0.0f;
+	}
+
+	// @brief  : 更新
+	//--------------------------------------------------------------------
+	void Update ()
+	{
+//		tra
+	}
+}
